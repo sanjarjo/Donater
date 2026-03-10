ee57ee2282e43750230ec9acbee1bfe3c3d4214c
sanjarjo <osanjar642@gmail.com>
2026-03-10 20:38:36 +0300

    Update and rename utils/order_id.py to backend/app/utils/order_id.py

---
+++ backend/app/utils/order_id.py
@@ -0,0 +1,5 @@
+import random
+
+def generate_order_id():
+    """6 raqamli noyob order raqami"""
+    return str(random.randint(100000, 999999))
--- utils/order_id.py
+++
@@ -1,5 +0,0 @@
-import random
-
-def generate_order_id():
-
-    return str(random.randint(100000,999999))
