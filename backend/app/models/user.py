afc33245e71a7b4d0eab4ae83757025b3d6844ae
sanjarjo <osanjar642@gmail.com>
2026-03-10 20:34:53 +0300

    Update and rename models/user.py to backend/app/models/user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -2,13 +2,10 @@ from sqlalchemy import Column, Integer, String
 from ..database import Base
 
 class User(Base):
-
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-
-    telegram_id = Column(String, unique=True)
-
+    telegram_id = Column(String, unique=True, index=True)
     username = Column(String)
-
     balance = Column(Integer, default=0)
+    role = Column(String, default="USER")  # USER, ADMIN, SUPER_ADMIN
